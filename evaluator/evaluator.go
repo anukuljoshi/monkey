d9b0d5425e3f0e96d46238418a44d8b21b4db23c
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-04-13 23:18:12 +0530

    eval funcs for prefix operators

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -23,10 +23,14 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBoolean(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalExpression(node.Operator, right)
 	}
 	return nil
 }
 
+// ast.Program helpers
 func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 
@@ -37,9 +41,44 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	return result
 }
 
+// ast.Boolean helpers
 func nativeBoolToBoolean(input bool) *object.Boolean {
 	if input {
 		return TRUE
 	}
 	return FALSE
+}
+
+// ast.Prefix helpers
+func evalExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
