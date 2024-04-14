9b64f21f77a68c3fd3e83ee27a800184bfe8fe61
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2024-04-14 18:39:05 +0530

    eval func for let statements and variable bindings

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/anukuljoshi/monkey/evaluator"
 	"github.com/anukuljoshi/monkey/lexer"
+	"github.com/anukuljoshi/monkey/object"
 	"github.com/anukuljoshi/monkey/parser"
 )
 
@@ -14,6 +15,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -29,7 +32,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
